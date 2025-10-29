48a8fba1e2c3eec5c5f056497df2103ad04be6cc
RiffatHussain <riffathussainkr@gmail.com>
2025-10-29 16:27:32 +0530

    Fix header title from '7th' to 'Seventh Gear'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,7 @@ export default function Header() {
 	return (
 		<div>
 			<header className="journal-header">
-<<<<<<< HEAD
 				<h1 className="title">Seventh Gear Travel Blogs</h1>
-=======
-				<h1 className="title">7th Gear Travel Blogs</h1>
->>>>>>> 4a640b99c37fa39d1151fe842ad040773f234213
 			</header>
 		</div>
 	)
