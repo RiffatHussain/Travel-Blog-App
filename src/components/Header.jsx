5ee883a84fa0d2ea05dbbd868a737695933fe735
RiffatHussain <riffathussainkr@gmail.com>
2025-10-29 15:11:05 +0530

    Update blog title in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 	return (
 		<div>
 			<header className="journal-header">
-				<h1 className="title">Seventh Gear Travel Blog</h1>
+				<h1 className="title">Seventh Gear Travel Blogs</h1>
 			</header>
 		</div>
 	)
