20fdbb9dc38505403a289106b6e3dee60329528d
RiffatHussain <riffathussainkr@gmail.com>
2025-10-29 16:05:51 +0530

    Update title from 'Seventh Gear' to '7th Gear'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 	return (
 		<div>
 			<header className="journal-header">
-				<h1 className="title">Seventh Gear Travel Blogs</h1>
+				<h1 className="title">7th Gear Travel Blogs</h1>
 			</header>
 		</div>
 	)
