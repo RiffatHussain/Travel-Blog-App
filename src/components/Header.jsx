8444829280ca3084a43a9d2937cd2d9dd3d896ae
RiffatHussain <riffathussainkr@gmail.com>
2025-10-31 11:27:13 +0530

    Testing Dev site pipline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,7 @@ export default function Header() {
 	return (
 		<div>
 			<header className="journal-header">
-				<h1 className="title">Seventh Gear Travel</h1>
-				<h1 className="title">This is the Staging Site for the Travel Blog</h1>
+				<h1 className="title">Dev Site is Updated</h1>
 			</header>
 		</div>
 	)
