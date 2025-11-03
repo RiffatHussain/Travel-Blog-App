fb37480eded249930773d24e132536c1cb4c4e69
RiffatHussain <riffathussainkr@gmail.com>
2025-11-03 12:49:17 +0530

    header is updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 	return (
 		<div>
 			<header className="journal-header">
-				<h1 className="title">Seventh Gear Trials</h1>
+				<h1 className="title">Seventh Gear Trials is going to be awesome</h1>
 			</header>
 		</div>
 	)
