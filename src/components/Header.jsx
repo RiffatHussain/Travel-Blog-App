c57c759ab3f32b7ff176650fe3f568359057332f
RiffatHussain <riffathussainkr@gmail.com>
2025-11-06 12:14:31 +0530

    Dev site Header is updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 	return (
 		<div>
 			<header className="journal-header">
-				<h1 className="title">Dev Site is still under construction</h1>
+				<h1 className="title">Dev Site pipeline testing 1</h1>
 			</header>
 		</div>
 	)
